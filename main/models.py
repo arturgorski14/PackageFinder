09d1ec2eee4d08b28b6bc5c2f68e918d0db02e5f
Artur Gorski <arturgorski14@gmail.com>
2023-03-31 23:06:31 +0200

    style: remove redundant code

--- main/models.py
+++ main/models.py
@@ -3,14 +3,3 @@ from django.db import models
 
 class PypiPackage(models.Model):
     description = models.CharField(max_length=512)
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
