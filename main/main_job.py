e3affb9466ad2774ab8f15f6204c36066094bbe1
Artur Gorski <arturgorski14@gmail.com>
2023-04-02 00:52:31 +0200

    cron working ...

--- main/main_job.py
+++ main/main_job.py
@@ -1,24 +1,18 @@
-import logging
-
-# from main.documents import PypiPackageDocument
 from main.models import PypiPackage
 from main.utils import create_soup
 from main.pypi_scrapper import PypiScrapper
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-
 
 def main_job():
-    log.info("MAIN JOB STARTS")
+    print("MAIN JOB STARTS")
     url = "https://pypi.org/rss/packages.xml"
     soup = create_soup(url)
 
     all_package_links = soup.find_all("link")
-    log.debug(all_package_links)
+    print(all_package_links)
     for tag in all_package_links[1:]:  # first is redundant
         package_url = tag.string
-        log.debug(package_url)
+        print(package_url)
 
         package_soup = create_soup(package_url)
         # author = PypiScrapper.find_author(package_soup)
@@ -33,11 +27,11 @@ def main_job():
 
         # print(f"{author=}\n{title=}\n{version=}\n{description=}\n{maintainer=}\n")
         save_data_to_elastic(title=title, version=version, description=description)
-    log.info("MAIN JOB ENDED")
+    print("MAIN JOB ENDED")
 
 
 def save_data_to_elastic(**kwargs):
-    log.info("Saving to elastic")
+    print("Saving to elastic")
     package = PypiPackage(**kwargs)
     package.save()
 
--- main/updater.py
+++ main/updater.py
@@ -5,6 +5,5 @@ from main.main_job import main_job
 def start():
     print("FROM MAIN CATALOGUE")
     scheduler = BackgroundScheduler()
-    # scheduler.add_job(main_job, 'interval', seconds=30)
-    scheduler.add_job(main_job, 'cron', hour=23, minute=15)
+    scheduler.add_job(main_job, 'cron', second=10, replace_existing=True)
     scheduler.start()
