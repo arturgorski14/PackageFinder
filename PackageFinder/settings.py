dd22cc5ed58a4a105e4b7b54dd41fa3305a264d3
Artur Gorski <arturgorski14@gmail.com>
2023-04-02 13:11:15 +0200

    Added django-bootstrap

--- PackageFinder/settings.py
+++ PackageFinder/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django_elasticsearch_dsl_drf",
 
     "main",
+    "django_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = "PackageFinder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
