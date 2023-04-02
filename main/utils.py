f9a9a2932e9c6d61d80e1b9ab3de09323e731ae2
Artur Gorski <arturgorski14@gmail.com>
2023-04-02 21:53:27 +0200

    fix: index looks better

--- main/utils.py
+++ main/utils.py
@@ -7,7 +7,7 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def get_response_content(url) -> bytes:
+def get_response_content(url: str) -> bytes:
     response = requests.get(url)
     if response.status_code != 200:
         log.warning(f"Received {response.status_code=} for url {url}")
@@ -16,5 +16,26 @@ def get_response_content(url) -> bytes:
 
 
 def create_soup(url: str) -> BeautifulSoup:
+    """
+    Create BeautifulSoup object for scrapper
+    xml option can process .html also
+    """
     response_data = get_response_content(url)
     return BeautifulSoup(response_data, "xml")
+
+
+def get_page_number(number) -> int:
+    """
+    Get page number for pagination.
+    Django paginator doesn't support Elasticsearch Query.
+    Function allows number > page_number
+    """
+    default_page_number = 1
+    try:
+        if isinstance(number, float) and not number.is_integer():
+            raise ValueError
+        number = int(number)
+    except (TypeError, ValueError):
+        return default_page_number
+
+    return max(number, default_page_number)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 import logging
+import os
+from math import ceil
+from typing import Optional
 
 from django.shortcuts import redirect, render
 from elasticsearch.exceptions import NotFoundError
@@ -6,22 +9,35 @@ from elasticsearch_dsl.query import MatchAll, MultiMatch
 
 from main.documents import PypiPackageDocument
 from main.main_job import main_job
+from main.utils import get_page_number
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
+PAGINATE_BY = int(os.environ.get("PAGINATE_BY", 10))
+
 
 def index(request):
     search_value = request.GET.get("q")
-    packages = get_data_from_elastic(search_value)
+    page_num = request.GET.get("page", 1)
+    page_num = get_page_number(page_num)
+
+    packages, p_count = get_paginated_data_from_elastic(search_value, page_num)
+    total_pages_num = ceil(p_count / PAGINATE_BY)
+    previous_page = max(1, page_num-1)
+    next_page = min(page_num + 1, total_pages_num)
 
     context = {
         "packages": packages,
+        "previous_page": previous_page,
+        "current_page": page_num,
+        "next_page": next_page,
+        "total_pages_num": total_pages_num
     }
     return render(request, "main/index.html", context)
 
 
-def get_data_from_elastic(search_value):
+def get_paginated_data_from_elastic(search_value: Optional[str], page_number: int):
     if search_value:
         match_query = MultiMatch(
             query=search_value,
@@ -29,14 +45,18 @@ def get_data_from_elastic(search_value):
         )
     else:
         match_query = MatchAll()
-    packages_query = PypiPackageDocument.search()[:100].query(match_query)
+
+    start = (page_number-1) * PAGINATE_BY
+    end = start + PAGINATE_BY
+
+    packages_query = PypiPackageDocument.search()[start:end].query(match_query)
 
     try:
         _ = packages_query.count()
     except NotFoundError:
-        return []
+        return [], 0
     else:
-        return packages_query
+        return packages_query, packages_query.count()
 
 
 def populate_index_on_demand(request):
